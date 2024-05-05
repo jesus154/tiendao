6d0128b8e9024244e631647d284d34a4b0565454
jesus154 <80117586+jesus154@users.noreply.github.com>
2024-05-04 19:05:35 -0600

    Add files via upload

---
+++ acerca/acerca.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - CSS + HTML only Accordion Element</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Titillium+Web:300,700,300italic'>
+<link rel="stylesheet" href="acerca.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<h1>Acerca de Classic Sports</h1>
+<ul>
+  <li>
+    <input type="checkbox" checked>
+    <i></i>
+    <h2>Historia de classic sports</h2>
+    <p>Classic Sports es una tienda de ropa deportiva fundada en 1990 por dos hermanos apasionados por el deporte y la moda. La idea surgió cuando los hermanos, que eran atletas en su juventud, se dieron cuenta de que no había una tienda que ofreciera una amplia gama de productos deportivos con un enfoque en la moda.
+        Comenzaron vendiendo ropa y accesorios deportivos en una pequeña tienda en el centro de la ciudad. La tienda se hizo popular rápidamente gracias a la calidad y variedad de los productos ofrecidos, así como a la atención personalizada que brindaban a cada cliente.
+        En poco tiempo, Classic Sports se expandió a varias ciudades y abrió varias sucursales. La tienda también comenzó a vender sus productos en línea, lo que aumentó significativamente su alcance y su base de clientes.
+        A lo largo de los años, Classic Sports ha colaborado con numerosos atletas profesionales y ha patrocinado varios eventos deportivos importantes. También ha lanzado su propia línea de ropa deportiva de alta calidad, que se ha convertido en una de las favoritas entre los atletas y los aficionados al deporte por igual.
+        Hoy en día, Classic Sports es una de las tiendas de ropa deportiva más grandes y reconocidas del país. A pesar de su crecimiento y éxito, los hermanos fundadores siguen comprometidos con su visión original de ofrecer ropa deportiva de alta calidad y estilo.</p>
+  </li>
+  <li>
+    <input type="checkbox" checked>
+    <i></i>
+    <h2>Valores</h2>
+    <p>Calidad: La calidad de los productos que ofrecemos es nuestra prioridad, siempre buscando ofrecer productos duraderos y que cumplan con las expectativas de nuestros clientes.
+        Pasión: Somos apasionados por el deporte y la moda, y eso se refleja en nuestra selección de productos y en la atención que brindamos a nuestros clientes.
+        Integridad: Nos comprometemos a ser honestos y éticos en todos nuestros negocios y relaciones con los clientes, proveedores y empleados.
+        Innovación: Estamos constantemente buscando nuevas formas de mejorar y ofrecer productos innovadores que satisfagan las necesidades de nuestros clientes.
+        Servicio al cliente: El servicio al cliente es una parte fundamental de nuestra filosofía, y nos esforzamos por brindar una atención personalizada y satisfacer las necesidades de cada cliente.
+        Trabajo en equipo: Fomentamos el trabajo en equipo y la colaboración para alcanzar nuestros objetivos y mejorar constantemente.
+        Responsabilidad social: Nos preocupamos por nuestro impacto en la sociedad y el medio ambiente, y nos esforzamos por ser una empresa socialmente responsable.</p>
+  </li>
+  <li>
+    <input type="checkbox" checked>
+    <i></i>
+    <h2>Mision</h2>
+    <p>La misión de Classic Sports es ofrecer a nuestros clientes una amplia gama de productos deportivos de alta calidad, con un enfoque en la moda y el estilo. Nos esforzamos por brindar una atención personalizada y un servicio excepcional al cliente en todas nuestras sucursales y en línea. 
+        Además, estamos comprometidos con la responsabilidad social y ambiental en todos nuestros negocios y relaciones con la comunidad. Como empresa, buscamos ser líderes en el mercado de la ropa deportiva y ser reconocidos por nuestra pasión por el deporte, la moda y el servicio al cliente.</p>
+  </li>
+  <li>
+    <input type="checkbox" checked>
+    <i></i>
+    <h2>Vision</h2>
+    <p>La visión de Classic Sports es ser la tienda de ropa deportiva líder en el mercado, reconocida por ofrecer productos de alta calidad y estilo, así como un servicio excepcional al cliente en todas nuestras sucursales y en línea. Además, nos esforzamos por ser una empresa socialmente responsable y ambientalmente sostenible, y ser un modelo a seguir en nuestra industria. Buscamos seguir creciendo y expandiéndonos a nivel nacional e internacional, y ser la opción preferida de los clientes que buscan ropa deportiva de alta calidad y estilo. En resumen, nuestra visión es ser la mejor opción para el deportista que busca calidad y estilo.</p>
+  </li>
+</ul>
+<!-- partial -->
+  
+</body>
+</html>
